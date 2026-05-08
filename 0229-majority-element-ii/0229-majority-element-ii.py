88a08e8f24e151201267ed429fa8e4d375d0abc5
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-05-08 14:51:37 +0530

    Time: 11 ms (35.74%), Space: 23.3 MB (36.71%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,14 +1,15 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
         hashSet = {}
+
         for num in nums:
-            hashSet[num] = 1 + hashSet.get(num, 0) 
-        n = len(nums) // 3
+            hashSet[num] = 1 + hashSet.get(num, 0)
+        
         res = []
-        for num, freq in hashSet.items():
-            if freq > n:
+        n = len(nums) // 3
+
+        for num, count in hashSet.items():
+            if count > n:
                 res.append(num)
         
-        return res
-
-        
+        return res 

b5a26316e7306138deaf75a88a028a60601e4bf3
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-05-09 08:51:15 +0530

    Time: 590 ms (12.96%), Space: 19.3 MB (55.05%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,30 +1,28 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        nums.sort()
         res = []
+        nums.sort()
         n = len(nums)
-
         for i in range(n-3):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
             for j in range(i+1,n-2):
                 if j > i+1 and nums[j] == nums[j-1]:
                     continue
-                left = j+1
-                right = n-1
-                while left < right:
-                    curr_sum = nums[i]+nums[j]+nums[left]+nums[right]
-                    if curr_sum == target:
-                        res.append([nums[i], nums[j], nums[left], nums[right]])
-                        left += 1
-                        right -= 1
-
-                        while left < right and nums[left] == nums[left-1]:
-                            left += 1
-                        while left < right and nums[right] == nums[right+1]:
-                            right -= 1
-                    elif curr_sum < target:
-                        left += 1
+                l = j+1
+                r = n-1
+                while l < r:
+                    if nums[i] + nums[j] + nums[l] + nums[r] == target:
+                        res.append([nums[i], nums[j], nums[l], nums[r]]) 
+                        while l < n-1 and nums[l] == nums[l+1]:
+                            l += 1
+                        l += 1
+                        while r > 0 and nums[r] == nums[r-1]:
+                            r -= 1
+                        r -= 1
+                    elif nums[i] + nums[j] + nums[l] + nums[r] < target:
+                        l += 1
                     else:
-                        right -= 1
+                        r -= 1
         return res
+                

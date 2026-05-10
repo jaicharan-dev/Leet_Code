3a1b137f68c4e1ca2be80627aee91a9e446c5dd1
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-05-10 10:47:05 +0530

    Time: 17 ms (65.62%), Space: 19.2 MB (87%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -10,17 +10,29 @@ class Solution:
             freqMap1[ord(s1[i]) - ord('a')] += 1
             freqMap2[ord(s2[i]) - ord('a')] += 1
         
-        if freqMap1 == freqMap2:
-            return True
-        
-        for j in range(len(s1), len(s2)):
-            i = j - len(s1)
-            freqMap2[ord(s2[i]) - ord('a')] -= 1
-            freqMap2[ord(s2[j]) - ord('a')] += 1
-            
-            if freqMap1 == freqMap2:
-                return True
-        
-        return False
+        matches = 0
+        for i in range(26):
+            if freqMap1[i] == freqMap2[i]:
+                matches += 1
 
- 
+
+        for i in range(len(s1), len(s2)):
+            if matches == 26:
+                return True
+            incoming_idx = ord(s2[i]) - ord('a')
+            freqMap2[incoming_idx] += 1
+            
+            if freqMap1[incoming_idx] == freqMap2[incoming_idx]:
+                matches += 1
+            if freqMap1[incoming_idx] + 1 == freqMap2[incoming_idx]:
+                matches -= 1
+            
+            outgoing_idx = ord(s2[i-len(s1)]) - ord('a')
+            freqMap2[outgoing_idx] -= 1
+
+            if freqMap1[outgoing_idx] == freqMap2[outgoing_idx]:
+                matches += 1
+            if freqMap1[outgoing_idx] - 1 == freqMap2[outgoing_idx]:
+                matches -= 1
+
+        return matches == 26

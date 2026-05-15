09fcaf52070aeb095beb23a6df18823ba8f66e0f
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-05-15 07:39:36 +0530

    Time: 3 ms (53.01%), Space: 19.4 MB (81.14%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -1,38 +1,30 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        freqMap = {}
-        for char in s:
-            freqMap[char] = 1 + freqMap.get(char, 0)
         
-        minHeap = []
-        for char, freq in freqMap.items():
-            heapq.heappush(minHeap, (-freq, char))
+        count = Counter(s)
         
-        res = ""
-        i = -1
-        while minHeap:
-            if i != -1 and res[i] != minHeap[0][1]:
-                freq, char = heapq.heappop(minHeap)
-                res += char
-                freq += 1
-            
-            elif i != -1 and res[i] == minHeap[0][1]:
-                if len(minHeap) == 1:
-                    return ""
-                else:
-                    freq1, char1 = heapq.heappop(minHeap)
-                    freq, char = heapq.heappop(minHeap)
-                    heapq.heappush(minHeap, (freq1, char1))
-                    res += char
-                    freq += 1
+        maxHeap = [[-freq, char] for char, freq in count.items()]
+        heapq.heapify(maxHeap)
 
-            else:
-                freq, char = heapq.heappop(minHeap)
-                res += char
-                freq += 1
-            
-            i += 1
+        prev = None
+        res = []
 
-            if freq < 0 :
-                heapq.heappush(minHeap, (freq, char))
-        return res
+        while maxHeap or prev:
+
+            if prev and not maxHeap:
+                return ""
+            
+            freq, char = heapq.heappop(maxHeap)
+            res.append(char)
+
+            if prev:
+                heapq.heappush(maxHeap, prev)
+                prev = None
+            
+            freq += 1
+            if freq != 0:
+                prev = [freq, char]
+            
+        
+        return "".join(res)
+

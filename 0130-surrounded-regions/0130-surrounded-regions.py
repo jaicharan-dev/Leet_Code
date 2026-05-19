d843851693720f2b687518f0105f9a5222d1499e
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-05-19 11:25:44 +0530

    Time: 1 ms (88.99%), Space: 22.3 MB (59.54%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -8,23 +8,27 @@ class Solution:
 
         for r in range(rows):
             if board[r][0] == "O":
+                board[r][0] = "T"
                 queue.append((r,0))
             if board[r][cols-1] == "O":
+                board[r][cols-1] = "T"
                 queue.append((r,cols-1))
             
         for c in range(cols):
             if board[0][c] == "O":
+                board[0][c] = "T"
                 queue.append((0,c))
             if board[rows-1][c] == "O":
+                board[rows-1][c] = "T"
                 queue.append((rows-1,c))
         
         directions = [(-1,0), (1,0), (0,-1), (0,1)]
         while queue:
             r, c = queue.popleft()
-            board[r][c] = "T"
             for dr, dc in directions:
                 nr, nc = r+dr, c+dc
                 if (0 <= nr < rows) and (0 <= nc < cols) and board[nr][nc] == "O":
+                    board[nr][nc] = "T"   
                     queue.append((nr, nc))
         
         for r in range(rows):
@@ -34,4 +38,3 @@ class Solution:
                 elif board[r][c] == "T":
                     board[r][c] = "O"
         
-        return board

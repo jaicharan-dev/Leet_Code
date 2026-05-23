149106a10ac668609557c6ee8c83c5da1a711916
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-05-23 15:29:47 +0530

    Time: 8 ms (29.26%), Space: 22.3 MB (59.23%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -5,35 +5,31 @@ class Solution:
         """
         rows, cols = len(board), len(board[0])
 
-        def dfs(r,c,initial_char,final_char):
-            if not (0 <= r < rows) or not (0 <= c < cols) or board[r][c] != initial_char:
+        def dfs(r,c):
+            if not (0 <= r < rows) or not (0 <= c < cols) or board[r][c] != "O":
                 return
             
-            board[r][c] = final_char
-            dfs(r+1,c,initial_char,final_char)
-            dfs(r-1,c,initial_char,final_char)
-            dfs(r,c+1,initial_char,final_char)
-            dfs(r,c-1,initial_char,final_char)
+            board[r][c] = "T"
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
 
         for r in range(rows):
             if board[r][0] == "O":
-                dfs(r,0,"O","T")
+                dfs(r,0)
             if board[r][cols-1] == "O":
-                dfs(r,cols-1,"O","T")
+                dfs(r,cols-1)
         
         for c in range(cols):
             if board[0][c] == "O":
-                dfs(0,c,"O","T")
+                dfs(0,c)
             if board[rows-1][c] == "O":
-                dfs(rows-1,c,"O","T")
+                dfs(rows-1,c)
         
         for r in range(rows):
             for c in range(cols):
                 if board[r][c] == "O":
                     board[r][c] = "X"
-        
-        for r in range(rows):
-            for c in range(cols):
                 if board[r][c] == "T":
-                    board[r][c] = "O"
-        
+                    board[r][c] = "O"  

8d117130fe41e54b7b8baf14186b6ed7d19a045c
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-05-24 11:20:33 +0530

    Time: 6 ms (50.44%), Space: 20.2 MB (93.15%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,32 +1,25 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         adj_list = defaultdict(list)
-        courses_taken = 0
         in_degree = [0] * numCourses
-
+        queue = deque()
+        courses_taken = 0
         for course, prereq in prerequisites:
             adj_list[prereq].append(course)
             in_degree[course] += 1
         
-        queue = deque()
         for course, freq in enumerate(in_degree):
             if freq == 0:
                 queue.append(course)
-        
+            
         while queue:
-            prereq = queue.popleft()
+            course = queue.popleft()
             courses_taken += 1
 
-            for next_course in adj_list[prereq]:
+            for next_course in adj_list[course]:
                 in_degree[next_course] -= 1
-
                 if in_degree[next_course] == 0:
                     queue.append(next_course)
         
         return courses_taken == numCourses
-
-        
-        
-
-
 

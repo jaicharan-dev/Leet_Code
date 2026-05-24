8a8db1368c5e8a768eef0faca5acdec35184ef03
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-05-24 12:04:55 +0530

    Time: 4 ms (47.9%), Space: 20.5 MB (55.75%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -2,23 +2,22 @@ class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         adj_list = defaultdict(list)
         in_degree = [0] * numCourses
-        
+        queue = deque()
+        res = []
         for course, prereq in prerequisites:
             adj_list[prereq].append(course)
             in_degree[course] += 1
-
-        queue = deque()
-        for course, freq in enumerate(in_degree):
-            if freq == 0:
+        
+        for course, degrees_needed in enumerate(in_degree):
+            if degrees_needed == 0:
                 queue.append(course)
-        res = []
+        
         while queue:
-            curr_course = queue.popleft()
-            res.append(curr_course)
+            course = queue.popleft()
+            res.append(course)
 
-            for next_course in adj_list[curr_course]:
+            for next_course in adj_list[course]:
                 in_degree[next_course] -= 1
-
                 if in_degree[next_course] == 0:
                     queue.append(next_course)
         

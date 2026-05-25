2d8b0a6e21617b2df66d715e54da65b9a7a772c5
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-05-25 14:49:33 +0530

    Time: 0 ms (100%), Space: 19.4 MB (78.14%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,7 +1,7 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:           
         # find pivot:
-        def pivot(nums):
+        def find_pivot(nums):
             l, r = 0, len(nums)-1
             while l < r:
                 if nums[l] < nums[r]:
@@ -24,10 +24,7 @@ class Solution:
                     r = mid - 1
             return -1
 
-        edge = pivot(nums)
-        if edge == 0:
-            return bs(0, len(nums)-1)
-        elif nums[0] <= target:
-            return bs(0, edge-1)
-        else:
-            return bs(edge, len(nums)-1)
+        pivot = find_pivot(nums)
+        if nums[pivot] <= target <= nums[-1]:
+            return bs(pivot, len(nums)-1)
+        return bs(0, pivot-1)

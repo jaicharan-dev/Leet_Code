60d96ac869dc9dee5d6a12ff113e5ded17e3782c
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-05-29 11:59:46 +0530

    Time: 12 ms (37.15%), Space: 23.1 MB (30.81%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -6,20 +6,20 @@
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         dummy = ListNode(0)
-        current = dummy
+        cur = dummy
         min_heap = []
-        
-        for index, linked_list_head, in enumerate(lists):
+
+        for idx, linked_list_head in enumerate(lists):
             if linked_list_head:
-                heapq.heappush(min_heap, (linked_list_head.val, index, linked_list_head))
-        
-        next_index = len(lists)
+                heapq.heappush(min_heap, (linked_list_head.val, idx, linked_list_head))
+        next_idx = len(lists)
+
         while min_heap:
             val, _, node = heapq.heappop(min_heap)
-            current.next = node
-            current = current.next
-            if node.next:
-                heapq.heappush(min_heap, (node.next.val, next_index, node.next))
-                next_index += 1
-        return dummy.next
+            cur.next = node
+            cur = cur.next
 
+            if node.next:
+                heapq.heappush(min_heap, (node.next.val, next_idx, node.next))
+                next_idx += 1
+        return dummy.next

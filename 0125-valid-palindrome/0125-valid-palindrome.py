8e7b3680a412020399874fcaac33cc2445844de7
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-06-01 12:05:18 +0530

    Time: 11 ms (33.7%), Space: 19.7 MB (56.34%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -2,21 +2,12 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         l, r = 0, len(s)-1
         while l < r:
-            while not self.alphanum(s[l]) and l < r:
+            while l < r and not s[l].isalnum():
                 l += 1
-            while not self.alphanum(s[r]) and l < r:
+            while l < r and not s[r].isalnum():
                 r -= 1
-            
             if s[l].lower() != s[r].lower():
                 return False
-            
             l += 1 
             r -= 1 
-
         return True
-
-    def alphanum(self, c):
-        if ord('a') <= ord(c) <= ord('z'): return True  
-        if ord('A') <= ord(c) <= ord('Z'): return True  
-        if ord('0') <= ord(c) <= ord('9'): return True
-        return False  

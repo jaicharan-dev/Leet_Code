8deb8fc2a26ae541609f3f062865b12aaff200ea
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-06-03 12:38:46 +0530

    Time: 0 ms (100%), Space: 19.4 MB (88.31%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -5,9 +5,9 @@ class Solution:
         nums.sort()
 
         def dfs(i):
-            if i == len(nums):
+            if i >= len(nums):
                 res.append(subset.copy())
-                return
+                return 
             
             subset.append(nums[i])
             dfs(i+1)
@@ -19,4 +19,3 @@ class Solution:
         
         dfs(0)
         return res
-

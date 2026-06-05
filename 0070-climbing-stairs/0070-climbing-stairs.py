5d20c1fdf7394436986e945765c1b1fe85038ee0
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-06-05 17:58:20 +0530

    Time: 0 ms (100%), Space: 19.2 MB (86.06%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,16 +1,14 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n == 1:
-            return 1
-        if n == 2:
-            return 2
+        if n <= 2:
+            return n
         
-        two_back = 1
         one_back = 2
+        two_back = 1
 
         for i in range(3, n+1):
-            current = one_back + two_back
+            curr_ways = one_back + two_back
             two_back = one_back
-            one_back = current
-
-        return one_back    
+            one_back = curr_ways
+        
+        return one_back

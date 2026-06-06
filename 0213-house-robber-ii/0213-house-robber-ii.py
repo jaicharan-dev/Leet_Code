27dd1247d15156432bf5e3092dc30899b475f14f
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-06-06 10:27:47 +0530

    Time: 0 ms (100%), Space: 19.3 MB (68.64%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -6,16 +6,12 @@ class Solution:
         def helper(street):
             rob1 = 0
             rob2 = 0
-            
             for n in street:
-                current = max(n+rob1, rob2)
+                curr = max(n+rob1, rob2)
                 rob1 = rob2
-                rob2 = current
-            
+                rob2 = curr
             return rob2
-        
-        street_a_max = helper(nums[:-1])
-        street_b_max = helper(nums[1:])
-
-        return max(street_a_max, street_b_max)
-        
+                
+        street_a = helper(nums[:-1])
+        street_b = helper(nums[1:])
+        return max(street_a, street_b)

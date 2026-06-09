731108afd76400cc3294e5ec0f22ff0f4038845d
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-06-09 14:00:39 +0530

    Time: 282 ms (55.86%), Space: 63.9 MB (33.03%) - LeetHub

---
+++ 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minInterval(self, intervals: List[List[int]], queries: List[int]) -> List[int]:
+        intervals.sort(key=lambda x: x[0])
+        sorted_queries = sorted((q,i) for i, q in enumerate(queries))
+
+        res = [-1] * len(queries)
+
+        active_intervals = []
+        i = 0
+
+        for query, original_index in sorted_queries:
+
+            while i < len(intervals) and intervals[i][0] <= query:
+                start, end = intervals[i]
+                size = end - start + 1
+                heapq.heappush(active_intervals, (size, end))
+                i += 1
+            
+            while active_intervals and active_intervals[0][1] < query:
+                heapq.heappop(active_intervals)
+            
+            if active_intervals:
+                res[original_index] = active_intervals[0][0]
+            
+        return res

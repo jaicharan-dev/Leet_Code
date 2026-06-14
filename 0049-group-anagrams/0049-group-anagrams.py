38451204bb70e094235f2025d9adf175114930d1
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-06-14 18:18:37 +0530

    Time: 15 ms (43.83%), Space: 23.9 MB (17.83%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,10 +1,11 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        wordMap = defaultdict(list)
-        for word in strs:
-            freqMap = [0] * 26
-            for char in word:
-                freqMap[ord(char) - ord('a')] += 1
+        hash_map = defaultdict(list)
+        for s in strs:
+            freq_map = [0] * 26  
+            for char in s:
+                freq_map[ord(char) - ord('a')] += 1  
+            hash_map[tuple(freq_map)].append(s)
 
-            wordMap[tuple(freqMap)].append(word)
-        return list(wordMap.values())
+        res = [char_list for char_list in hash_map.values()]
+        return res

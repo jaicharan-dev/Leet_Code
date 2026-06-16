c3d9de73309cee1a7ed7a0e2c20b2c5a5b89f1ac
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-06-16 10:58:45 +0530

    Time: 53 ms (69.08%), Space: 19.5 MB (43.06%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -3,20 +3,17 @@ class Solution:
         l, r = 0, len(s)-1
         while l < r:
             if s[l] != s[r]:
-                return self.pali(s,l+1,r) or self.pali(s,l,r-1)
-            l += 1
-            r -= 1
+                return self.is_pali(s, l+1,r) or self.is_pali(s, l, r-1)
+            else:
+                l += 1
+                r -= 1
         return True
-
-    def pali(self, s, l, r):
+    
+    def is_pali(self, s, l, r):
         while l < r:
             if s[l] != s[r]:
                 return False
-            l += 1
-            r -= 1
+            else:
+                l += 1
+                r -= 1
         return True
-
-
-            
-
-

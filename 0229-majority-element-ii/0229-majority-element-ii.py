388ab464af456ad15c9ec7c6a787aa1e70bfbfee
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-06-17 11:35:13 +0530

    Time: 7 ms (67%), Space: 22.1 MB (56.59%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,15 +1,37 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        hashSet = {}
+        if not nums: return []
+        curr1, curr2 = None, None
+        cnt1, cnt2 = 0, 0
 
-        for num in nums:
-            hashSet[num] = 1 + hashSet.get(num, 0)
+        for n in nums:
+            if n == curr1:
+                cnt1 += 1
+            elif n == curr2:
+                cnt2 += 1
+            elif cnt1 == 0:
+                curr1 = n
+                cnt1 = 1
+            elif cnt2 == 0:
+                curr2 = n
+                cnt2 = 1
+            else:
+                cnt1 -= 1
+                cnt2 -= 1
+
+        cnt1, cnt2 = 0, 0
+        for n in nums:
+            if curr1 == n:
+                cnt1 += 1
+            elif curr2 == n:
+                cnt2 += 1
         
         res = []
-        n = len(nums) // 3
+        cnt_needed = len(nums) // 3
 
-        for num, count in hashSet.items():
-            if count > n:
-                res.append(num)
+        if cnt1 > cnt_needed:
+            res.append(curr1)
+        if cnt2 > cnt_needed:
+            res.append(curr2)
         
-        return res 
+        return res

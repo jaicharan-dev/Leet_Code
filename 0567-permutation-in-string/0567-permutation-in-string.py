a8981369cf3203eb7a7161300bedef5fa54dd497
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-06-18 12:50:38 +0530

    Time: 7 ms (99.78%), Space: 19.4 MB (18.7%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,38 +1,24 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2):
-            return False
+        n1, n2 = len(s1), len(s2)
+        if n1 > n2: return False
 
-        freqMap1 = [0] * 26        
-        freqMap2 = [0] * 26
+        freq1 = [0] * 26
+        freq2 = [0] * 26
 
-        for i in range(len(s1)):
-            freqMap1[ord(s1[i]) - ord('a')] += 1
-            freqMap2[ord(s2[i]) - ord('a')] += 1
+        for i in range(n1):
+            freq1[ord(s1[i]) - ord('a')] += 1
+            freq2[ord(s2[i]) - ord('a')] += 1
         
-        matches = 0
-        for i in range(26):
-            if freqMap1[i] == freqMap2[i]:
-                matches += 1
+        if freq1 == freq2:
+            return True
+        
+        for j in range(n1, n2):
+            freq2[ord(s2[j]) - ord('a')] += 1
+            freq2[ord(s2[j - n1]) - ord('a')] -= 1
 
-
-        for i in range(len(s1), len(s2)):
-            if matches == 26:
+            if freq1 == freq2:
                 return True
-            incoming_idx = ord(s2[i]) - ord('a')
-            freqMap2[incoming_idx] += 1
             
-            if freqMap1[incoming_idx] == freqMap2[incoming_idx]:
-                matches += 1
-            if freqMap1[incoming_idx] + 1 == freqMap2[incoming_idx]:
-                matches -= 1
-            
-            outgoing_idx = ord(s2[i-len(s1)]) - ord('a')
-            freqMap2[outgoing_idx] -= 1
-
-            if freqMap1[outgoing_idx] == freqMap2[outgoing_idx]:
-                matches += 1
-            if freqMap1[outgoing_idx] - 1 == freqMap2[outgoing_idx]:
-                matches -= 1
-
-        return matches == 26
+        return False
+        

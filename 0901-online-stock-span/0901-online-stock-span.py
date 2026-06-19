5989a2b8b6f0b1ce0b0e2d0c5cdf8bf3fc603149
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-06-19 12:34:58 +0530

    Time: 95 ms (8.09%), Space: 25.3 MB (6.78%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.py
+++ 0901-online-stock-span/0901-online-stock-span.py
@@ -1,19 +1,17 @@
 class StockSpanner:
 
     def __init__(self):
-        self.stack = []
+        self.arr = []
 
     def next(self, price: int) -> int:
         span = 1
-        while self.stack and price >= self.stack[-1][0]:
-            _, prev_span = self.stack.pop()
-            span += prev_span
-        self.stack.append((price, span))
+        i = len(self.arr) - 1
+        while self.arr and i >= 0 and price >= self.arr[i][0]:
+            span += self.arr[i][1]
+            i -= self.arr[i][1]
 
+        self.arr.append((price, span))
         return span
-
-        
-
 
 
 # Your StockSpanner object will be instantiated and called as such:

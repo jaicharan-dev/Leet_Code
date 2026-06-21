026af8280d0fa14d1f24a281e0844e5c6de7743f
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-06-21 14:57:06 +0530

    Time: 49 ms (48.47%), Space: 20.2 MB (19.48%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -8,63 +8,21 @@ class Node:
 """
 
 class Solution:
-    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':     
-        '''
-        if not head:
-            return None
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        old_to_new = { None:None }
+        curr = head
+        while curr:
+            if not curr in old_to_new:
+                old_to_new[curr] = Node(curr.val)
+            curr = curr.next
         
-        # 1. Interleave cloned nodes within the original list
-        cur = head
-        while cur:
-            nxt = cur.next
-            copy = Node(cur.val)
-            cur.next = copy
-            copy.next = nxt
-            cur = nxt
-            
-        # 2. Assign random pointers to the cloned nodes
-        cur = head
-        while cur:
-            if cur.random:
-                cur.next.random = cur.random.next
-            cur = cur.next.next
-            
-        # 3. Separate the interleaved lists
-        cur = head
-        dummy = Node(0)
-        copy_curr = dummy
-        
-        while cur:
-            nxt = cur.next.next
-            
-            # Extract the copy
-            copy = cur.next
-            copy_curr.next = copy
-            copy_curr = copy
-            
-            # Restore the original link
-            cur.next = nxt
-            cur = nxt
-            
-        return dummy.next
-        '''
-        
-        cur = head
-        old_to_new = {None:None}
+        curr = head
+        while curr:
+            copy = old_to_new[curr]
+            copy.next = old_to_new[curr.next]
+            copy.random = old_to_new[curr.random]
+            curr = curr.next
 
-        while cur:
-            if cur not in old_to_new:
-                copy = Node(cur.val)
-                old_to_new[cur] = copy
-            cur = cur.next
-        
-        cur = head
-        while cur:
-            copy = old_to_new[cur]
-            copy.next = old_to_new[cur.next]
-            copy.random = old_to_new[cur.random]
-            cur = cur.next
-        
         return old_to_new[head]
-
         
+

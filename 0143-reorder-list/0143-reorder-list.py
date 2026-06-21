5cc005daea50ecdeddc9a4e8da2b9fee6159a0fc
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-06-21 14:10:50 +0530

    Time: 6 ms (19.87%), Space: 27.8 MB (46.3%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -8,30 +8,31 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        if not head or not head.next or not head.next.next:
-            return
-        
-        slow, fast = head, head
-        while fast.next and fast.next.next:
-            fast = fast.next.next
+        dummy = ListNode()
+        dummy.next = head
+
+        slow, fast = dummy, dummy
+        while fast and fast.next:
             slow = slow.next
+            fast = fast.next.next
         
         curr = slow.next
         slow.next = None
-
         prev = None
         while curr:
             curr.next, prev, curr = prev, curr, curr.next
         
         head2 = prev
-        head1 = head
+        head1 = dummy.next
 
         while head2:
             temp1 = head1.next
             temp2 = head2.next
-            
+
             head1.next = head2
             head2.next = temp1
-            
+
             head1 = temp1
             head2 = temp2
+
+        return dummy.next

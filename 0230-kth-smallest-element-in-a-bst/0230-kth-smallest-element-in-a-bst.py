fc0567e935474374efc620a4f898f49d4f372a85
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-06-23 12:43:30 +0530

    Time: 1 ms (38.45%), Space: 22.4 MB (22.52%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -6,16 +6,11 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        max_heap = []
+        res = []
         def dfs(node):
             if not node: return
-            if len(max_heap) < k:
-                heapq.heappush(max_heap, -node.val)
-            else:
-                heapq.heappushpop(max_heap, -node.val)
-
             dfs(node.left)
+            res.append(node.val)
             dfs(node.right)
-
         dfs(root)
-        return -max_heap[0]
+        return res[k-1]

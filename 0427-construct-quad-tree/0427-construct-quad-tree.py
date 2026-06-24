3c0187724be9d4605f54717b1aa6fe10d85c528d
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-06-24 12:17:54 +0530

    Time: 88 ms (82.17%), Space: 20.1 MB (71.71%) - LeetHub

--- 0427-construct-quad-tree/0427-construct-quad-tree.py
+++ 0427-construct-quad-tree/0427-construct-quad-tree.py
@@ -15,6 +15,7 @@ class Solution:
         def build(r, c, length):
             first_val = grid[r][c]
             is_uniform = True
+
             for i in range(length):
                 for j in range(length):
                     if grid[r+i][c+j] != first_val:
@@ -24,17 +25,18 @@ class Solution:
                     break
             
             if is_uniform:
-                return Node(val=(first_val==1), isLeaf=True,
-                            topLeft=None, topRight=None, 
-                            bottomLeft=None, bottomRight=None)
+                return Node(val = (first_val == 1), isLeaf = True,
+                            topLeft = None, topRight = None,
+                            bottomLeft = None, bottomRight = None)
             
             half = length // 2
-            topLeft = build(r, c, half)
-            topRight = build(r, c+half, half)
-            bottomLeft = build(r+half, c, half) 
-            bottomRight = build(r+half, c+half, half)
 
-            return Node(val=True, isLeaf=False,
-                        topLeft=topLeft, topRight=topRight, 
-                        bottomLeft=bottomLeft, bottomRight=bottomRight)
+            topLeft = build(r, c, half)
+            topRight = build(r, c + half, half)
+            bottomLeft = build(r + half, c, half)
+            bottomRight = build(r + half, c + half, half)
+
+            return Node(val = True, isLeaf = False,
+                        topLeft = topLeft, topRight = topRight,
+                        bottomLeft = bottomLeft, bottomRight = bottomRight)
         return build(0, 0, len(grid))

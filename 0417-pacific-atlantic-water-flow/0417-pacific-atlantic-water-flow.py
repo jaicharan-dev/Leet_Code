ebf0e9f7435c9de0d9372c71e2aefb6683b11418
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-06-25 17:32:37 +0530

    Time: 26 ms (76.71%), Space: 22.3 MB (37.87%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,40 +1,34 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        if not heights or not heights[0]: return []
-        
         pac_set, atl_set = set(), set()
-        pac_q, atl_q = deque(), deque()
         rows, cols = len(heights), len(heights[0])
 
+        def dfs(r, c, ocean_set, prev_height):
+
+            if (not (0 <= r < rows) or
+                not (0 <= c < cols) or 
+                prev_height > heights[r][c] or
+                (r,c) in ocean_set):
+                return 
+            
+            ocean_set.add((r,c))
+
+            dfs(r+1, c, ocean_set, heights[r][c])
+            dfs(r-1, c, ocean_set, heights[r][c])
+            dfs(r, c+1, ocean_set, heights[r][c])
+            dfs(r, c-1, ocean_set, heights[r][c])
+            return
+        
         for r in range(rows):
-            pac_q.append((r,0))
-            pac_set.add((r,0))
-            atl_q.append((r,cols-1))
-            atl_set.add((r,cols-1))
+            dfs(r, 0, pac_set, 0)
+            dfs(r, cols-1, atl_set, 0)
         
         for c in range(cols):
-            if (0,c) not in pac_set:
-                pac_q.append((0,c))
-                pac_set.add((0,c))
-            if (rows-1,c) not in atl_set:
-                atl_q.append((rows-1,c))
-                atl_set.add((rows-1,c))
+            dfs(0, c, pac_set, 0)
+            dfs(rows-1, c, atl_set, 0)
         
-        def bfs(queue, visit_set):
-            directions = [(1,0), (-1,0), (0,1), (0,-1)]
-            while queue:
-                r, c = queue.popleft()
-                for dr, dc in directions:
-                    nr, nc = r+dr, c+dc
-                    if (0 <= nr < rows and 0 <= nc < cols and 
-                    (nr,nc) not in visit_set and heights[nr][nc] >= heights[r][c]):
-                        queue.append((nr,nc))
-                        visit_set.add((nr,nc))
+        return list(pac_set & atl_set)
+
         
-        bfs(pac_q, pac_set)
-        bfs(atl_q, atl_set)
-
-        return [list(cell) for cell in (pac_set & atl_set)]
-
-
+        
 

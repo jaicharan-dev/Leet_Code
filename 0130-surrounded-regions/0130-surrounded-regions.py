0e13e3c12799cc4dbce91ec6ab1e978c689071bb
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-06-26 10:37:08 +0530

    Time: 64 ms (5.2%), Space: 33.5 MB (5.16%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -3,33 +3,35 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
+        
         rows, cols = len(board), len(board[0])
 
-        def dfs(r,c):
-            if not (0 <= r < rows) or not (0 <= c < cols) or board[r][c] != "O":
-                return
+        def dfs(r, c, visit):
+            if (not (0 <= r < rows) or 
+                not (0 <= c < cols)):
+                return False
             
-            board[r][c] = "T"
-            dfs(r+1,c)
-            dfs(r-1,c)
-            dfs(r,c+1)
-            dfs(r,c-1)
-
-        for r in range(rows):
-            if board[r][0] == "O":
-                dfs(r,0)
-            if board[r][cols-1] == "O":
-                dfs(r,cols-1)
-        
-        for c in range(cols):
-            if board[0][c] == "O":
-                dfs(0,c)
-            if board[rows-1][c] == "O":
-                dfs(rows-1,c)
-        
+            if board[r][c] == "X":
+                return True
+            
+            if board[r][c] == "O" and (r,c) in visit:
+                return True 
+            
+            if board[r][c] == "O" and (r,c) not in visit:
+                visit.add((r, c))
+                return (dfs(r+1, c, visit) and
+                        dfs(r-1, c, visit) and
+                        dfs(r, c+1, visit) and
+                        dfs(r, c-1, visit))
+         
         for r in range(rows):
             for c in range(cols):
                 if board[r][c] == "O":
-                    board[r][c] = "X"
-                if board[r][c] == "T":
-                    board[r][c] = "O"  
+                    visit_set = set()
+                    if dfs(r, c, visit_set):
+                        for vr, vc in visit_set:
+                            board[vr][vc] = "X"
+        
+                     
+
+

a1e5cd03230dd6b4f5789750b68f5bcc1bec5f73
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-06-27 12:05:23 +0530

    Time: 47 ms (92.26%), Space: 22.5 MB (23.61%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -5,26 +5,24 @@ class Solution:
         
         adj_list = defaultdict(list)
         for word in wordList:
-            for i in range(len(word)):            
+            for i in range(len(word)):
                 pattern = word[:i] + "*" + word[i+1:]
                 adj_list[pattern].append(word)
 
-        queue = deque([(beginWord,1)])
+        queue = deque([(beginWord, 1)])
         visited = set([beginWord])
 
         while queue:
-            current_word, steps = queue.popleft()
-            
-            if current_word == endWord:
+            curr_word, steps = queue.popleft()
+            if curr_word == endWord:
                 return steps
-
-            for i in range(len(current_word)):
-                pattern = current_word[:i] + "*" + current_word[i+1:]
+            
+            for i in range(len(curr_word)):
+                pattern = curr_word[:i] + "*" + curr_word[i+1:]
                 for neighbor in adj_list[pattern]:
                     if neighbor not in visited:
                         visited.add(neighbor)
-                        queue.append((neighbor,steps+1))
+                        queue.append((neighbor, steps+1))
                 
                 adj_list[pattern] = []
-        
         return 0

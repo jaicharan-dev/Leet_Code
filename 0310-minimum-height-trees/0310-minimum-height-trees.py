e972253bb705e403c63c5f21d5a1651b016f6b24
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-06-27 12:45:08 +0530

    Time: 56 ms (40.69%), Space: 27.9 MB (66.25%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.py
+++ 0310-minimum-height-trees/0310-minimum-height-trees.py
@@ -1,7 +1,5 @@
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
-        if n == 1:
-            return [0]
         
         adj_list = defaultdict(list)
         in_degree = [0] * n
@@ -11,23 +9,21 @@ class Solution:
             adj_list[v].append(u)
             in_degree[u] += 1
             in_degree[v] += 1
-
+        
         leaves = deque()
         for i in range(n):
-            if in_degree[i] == 1:
+            if in_degree[i] == 1 or in_degree[i] == 0:
                 leaves.append(i)
         
-        remaining_nodes = n 
-        while remaining_nodes > 2:
-            remaining_nodes -= len(leaves)
-            
+        nodes_left = n
+        while nodes_left > 2:
+            nodes_left -= len(leaves)
+
             for _ in range(len(leaves)):
                 leaf = leaves.popleft()
-                
                 for neighbor in adj_list[leaf]:
                     in_degree[neighbor] -= 1
                     if in_degree[neighbor] == 1:
                         leaves.append(neighbor)
-                
         return list(leaves)
 

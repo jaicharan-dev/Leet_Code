ac65d0003b1f747f7174e1162cd5517a9199ab6a
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-06-27 11:08:47 +0530

    Time: 3 ms (99.29%), Space: 19.6 MB (98.71%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.py
+++ 1462-course-schedule-iv/1462-course-schedule-iv.py
@@ -1,29 +1,34 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        
         adj_list = defaultdict(list)
         in_degree = [0] * numCourses
-        prereq_sets = [set() for _ in range(numCourses)]
+        prereq_set = defaultdict(set)
+        q = deque()
 
-        for prereq, course in prerequisites:
-            adj_list[prereq].append(course)
-            in_degree[course] += 1
-            prereq_sets[course].add(prereq)
+        for prereq, crs in prerequisites:
+            adj_list[prereq].append(crs)
+            in_degree[crs] += 1
+            prereq_set[crs].add(prereq)
         
-        queue = deque()
-        for i in range(numCourses):
-            if in_degree[i] == 0:
-                queue.append(i)
-
-        while queue:
-            curr_course = queue.popleft()
-
-            for next_course in adj_list[curr_course]:
-                prereq_sets[next_course].update(prereq_sets[curr_course])
-                in_degree[next_course] -= 1
-                if in_degree[next_course] == 0:
-                    queue.append(next_course)
+        for crs, prereq in enumerate(in_degree):
+            if prereq == 0:
+                q.append(crs)
         
+        while q:
+            crs = q.popleft()
+            for next_crs in adj_list[crs]:
+                prereq_set[next_crs].update(prereq_set[crs])
+                in_degree[next_crs] -= 1
+
+                if in_degree[next_crs] == 0:
+                    q.append(next_crs)
+
         res = []
-        for u,v in queries:
-            res.append(u in prereq_sets[v])
-        return res
+
+        for u, v in queries:
+            res.append(u in prereq_set[v])
+        return res 
+
+
+

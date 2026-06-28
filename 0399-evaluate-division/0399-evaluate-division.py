63f9fecab55226c3fd1d1e0b1540473e69d0da94
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-06-28 14:57:53 +0530

    Time: 0 ms (100%), Space: 19.6 MB (38.67%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,19 +1,21 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         graph = defaultdict(dict)
+
         for (num, deno), val in zip(equations, values):
             graph[num][deno] = val
             graph[deno][num] = 1.0/val
-
+        
         def dfs(curr, target, visit):
-            if curr not in graph or target not in graph:
+            if not curr in graph or target not in graph:
                 return -1.0
+            
             if curr == target:
                 return 1.0
-            
-            visit.add(curr)
+
             for neighbor, weight in graph[curr].items():
                 if neighbor not in visit:
+                    visit.add(neighbor)
                     product = dfs(neighbor, target, visit)
                     if product != -1.0:
                         return weight * product

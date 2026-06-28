e8ad7474780bde4f9c31b8edfd3ec27a5921d690
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-06-28 15:10:18 +0530

    Time: 335 ms (66.21%), Space: 20.5 MB (27.65%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.py
+++ 0752-open-the-lock/0752-open-the-lock.py
@@ -1,29 +1,30 @@
 class Solution:
     def openLock(self, deadends: List[str], target: str) -> int:
         visited = set(deadends)
-        if "0000" in visited: return -1
-        queue = deque([("0000", 0)])
+        if "0000" in visited:
+            return -1
+            
         visited.add("0000")
-
+        queue = deque([("0000", 0)])
+        
         while queue:
             state, turns = queue.popleft()
             if state == target:
                 return turns
-            
             for i in range(4):
                 digit = int(state[i])
+
                 up_digit = str((digit + 1) % 10)
                 down_digit = str((digit - 1) % 10)
-                
+
                 up_state = state[:i] + up_digit + state[i+1:]
                 down_state = state[:i] + down_digit + state[i+1:]
 
                 if up_state not in visited:
                     visited.add(up_state)
                     queue.append((up_state, turns+1))
-
+                
                 if down_state not in visited:
                     visited.add(down_state)
                     queue.append((down_state, turns+1))
-                
         return -1

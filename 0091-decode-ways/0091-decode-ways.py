d8605ff8ef08e6619f7a7b2cd7878d7807a0a2ba
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-06-30 19:56:02 +0530

    Time: 0 ms (100%), Space: 19.3 MB (40.59%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,11 +1,11 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        if s[0] == "0":
+        if s[0] == "0": 
             return 0
 
         two_back = 1
         one_back = 1
-                
+
         for i in range(1, len(s)):
             current = 0
             if s[i] != "0":
@@ -13,8 +13,8 @@ class Solution:
             
             if 10 <= int(s[i-1:i+1]) <= 26:
                 current += two_back
-
+            
             two_back = one_back
             one_back = current
-
-        return one_back 
+        
+        return one_back

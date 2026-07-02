edcc14c03e568b49db3109949f567c68022e17d5
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-07-02 11:56:17 +0530

    Time: 188 ms (76.89%), Space: 57.5 MB (17.7%) - LeetHub

--- 2402-meeting-rooms-iii/2402-meeting-rooms-iii.py
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.py
@@ -2,30 +2,29 @@ class Solution:
     def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
         meetings.sort(key=lambda x: x[0])
         room_counts = [0] * n
-        
+
         available_rooms = [i for i in range(n)]
         heapq.heapify(available_rooms)
 
         busy_rooms = []
 
         for start, end in meetings:
-
             while busy_rooms and busy_rooms[0][0] <= start:
-                finish_time, room_num = heapq.heappop(busy_rooms)
-                heapq.heappush(available_rooms, room_num)
-            
+                end_time, room_no = heapq.heappop(busy_rooms)
+                heapq.heappush(available_rooms, room_no)
+
             if available_rooms:
-                room_num = heapq.heappop(available_rooms)
-                heapq.heappush(busy_rooms, (end, room_num))
-                room_counts[room_num] += 1
-        
-            else:
-                earliest_end, room_num = heapq.heappop(busy_rooms)
-                duration = end - start
-                new_end = duration + earliest_end
-                heapq.heappush(busy_rooms, (new_end, room_num))
-                room_counts[room_num] += 1
+                room = heapq.heappop(available_rooms)
+                heapq.heappush(busy_rooms, (end, room))
+                room_counts[room] += 1
             
+            else:
+                nearest_end, room = heapq.heappop(busy_rooms)
+                duration = end - start
+                new_end = nearest_end + duration
+                heapq.heappush(busy_rooms, (new_end, room))
+                room_counts[room] += 1
+        
         max_meetings = -1
         best_room = -1
 
@@ -35,4 +34,4 @@ class Solution:
                 best_room = i
         
         return best_room
-
+        

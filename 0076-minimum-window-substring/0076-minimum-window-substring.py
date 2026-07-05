e9dc6e1511e7b36ea9caa1599715cdaa5d75383a
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-07-05 14:26:53 +0530

    Time: 59 ms (86.35%), Space: 19.7 MB (36.73%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,35 +1,27 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if not s or not t: return ""
-
         target_dict = Counter(t)
         need = len(target_dict)
-
         curr_dict = {char:0 for char in target_dict.keys()}
-        have = 0
-
-        res_length = float("inf")
+        have = 0 
+        res_len = float('inf')
         res_start = 0
-
-        left = 0
-        for right in range(len(s)):
-            char = s[right]
-            if char in curr_dict:
-                curr_dict[char] += 1
-                if curr_dict[char] == target_dict[char]:
-                    have += 1
-                
-            while have == need:
-                if res_length > (right-left+1):
-                    res_start = left
-                    res_length = right-left+1
-                
-                left_char = s[left]
-                if left_char in curr_dict:
-                    curr_dict[left_char] -= 1
-                    if curr_dict[left_char] < target_dict[left_char]:
-                        have -= 1
-                
-                left += 1
-        return s[res_start:res_start+res_length] if res_length != float('inf') else ""
         
+        l = 0
+        for r in range(len(s)):
+            if s[r] in curr_dict:
+                curr_dict[s[r]] += 1
+                if curr_dict[s[r]] == target_dict[s[r]]:
+                    have += 1
+
+                while have == need:
+                    if res_len > r-l+1:
+                        res_len = r-l+1
+                        res_start = l
+                    if s[l] in curr_dict:
+                        curr_dict[s[l]] -= 1
+                        if curr_dict[s[l]] < target_dict[s[l]]:
+                            have -= 1
+                    l += 1
+        return s[res_start: res_start+res_len] if res_len != float('inf') else ""
+                

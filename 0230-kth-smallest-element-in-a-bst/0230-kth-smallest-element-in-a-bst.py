d0a3c726c0b3032833d7775063def70c667be979
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-07-07 09:57:23 +0530

    Time: 0 ms (100%), Space: 22.2 MB (39%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -9,15 +9,14 @@ class Solution:
         stack = []
         curr = root
 
-        while stack or curr:
+        while curr or stack:
             while curr:
                 stack.append(curr)
                 curr = curr.left
-            
+
             curr = stack.pop()
             k -= 1
-
             if k == 0:
                 return curr.val
-            
-            curr = curr.right
+            else:
+                curr = curr.right

7b7378ccab6527d686a3845b04139d10a7e4b154
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-07-11 10:18:14 +0530

    Time: 0 ms (100%), Space: 19.3 MB (31.65%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,24 +1,20 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n = len(nums)
-        rob_last = self.helper(nums[1:])
-        skip_last = self.helper(nums[:n-1])
+        if len(nums) == 1: return nums[0]
+        rob1 = self.dp(nums[:-1])
+        rob2 = self.dp(nums[1:])
 
-        return max(rob_last, skip_last, nums[0])
-    
-    def helper(self, nums):
-        rob1 = 0
-        rob2 = 0
+        return max(rob1, rob2)
 
-        for n in nums:
-            rob_this = (n+rob2)
-            rob_not = rob1
+    def dp(self, houses):
+        if len(houses) == 1: return houses[0]
+        if len(houses) == 2: return max(houses[0], houses[1])
 
-            rob2 = rob1
-            rob1 = max(rob_this, rob_not)
+        two_back, one_back = houses[0], max(houses[0], houses[1])
 
-        return rob1
+        for i in range(2, len(houses)):
+            curr = max(two_back + houses[i], one_back)
+            two_back = one_back
+            one_back = curr
         
-
-
-
+        return one_back

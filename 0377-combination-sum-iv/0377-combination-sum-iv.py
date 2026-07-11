2d261db1e159d8a6d6b0cffbcacbc3cc88977573
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-07-11 21:13:01 +0530

    Time: 51 ms (51.65%), Space: 19.1 MB (91.17%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -0,0 +1,17 @@
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        memo = {}
+
+        def dfs(i):
+            if i in memo: return memo[i]
+            if i == 0: return 1
+            if i < 0: return 0
+
+            total_ways = 0
+            for num in nums:
+                total_ways += dfs(i-num)
+            
+            memo[i] = total_ways
+            return memo[i]
+        
+        return dfs(target)

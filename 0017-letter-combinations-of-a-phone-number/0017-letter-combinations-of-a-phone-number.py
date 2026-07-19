c0985973f8f0930d669a01a20ec54798ea6394a6
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-07-19 07:55:48 +0530

    Time: 0 ms (100%), Space: 19.4 MB (9.06%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,24 +1,25 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         if not digits: return []
+
         keyboard = {
-            '2':"abc", '3':"def", '4':"ghi", '5':"jkl",
-            '6':"mno", '7':"pqrs", '8': "tuv", '9': "wxyz"
+            "2":"abc", "3":"def", "4":"ghi", "5":"jkl", 
+            "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"
         }
 
         res = []
-        
+
         def dfs(i, path):
             if len(path) == len(digits):
                 res.append("".join(path))
                 return
             
             letters = keyboard[digits[i]]
-            
+
             for letter in letters:
                 path.append(letter)
                 dfs(i+1, path)
                 path.pop()
-        
+            
         dfs(0, [])
         return res

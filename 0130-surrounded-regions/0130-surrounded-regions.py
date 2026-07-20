55ce119e0af6b73beda44d83678a86ec0c56f1dc
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-07-20 10:59:04 +0530

    Time: 31 ms (13.07%), Space: 29 MB (9.5%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -3,35 +3,40 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        
         rows, cols = len(board), len(board[0])
-        global_set = set() 
-
-        def dfs(r, c, cur_set):
-            if (not (0 <= r < rows) or not (0 <= c < cols)):
-                return True
+        
+        def dfs(r, c, target, final):
+            if (not (0 <= r < rows) or
+                not (0 <= c < cols) or
+                board[r][c] != target):
+                return
             
-            if board[r][c] == "X" or (r,c) in global_set:
-                return False
-            
-            global_set.add((r, c))
-            cur_set.append((r, c))
-
-            up = dfs(r+1, c, cur_set)
-            down = dfs(r-1, c, cur_set)
-            left = dfs(r, c+1, cur_set)
-            right = dfs(r, c-1, cur_set)
-
-            return up or down or left or right
-         
+            board[r][c] = final
+            dfs(r+1, c, target, final)
+            dfs(r-1, c, target, final)
+            dfs(r, c+1, target, final)
+            dfs(r, c-1, target, final)
+            return
+        
         for r in range(rows):
             for c in range(cols):
-                if board[r][c] == "O" and (r,c) not in global_set:
-                    cur_set = []
-                    if not dfs(r, c, cur_set):
-                        for vr, vc in cur_set:
-                            board[vr][vc] = "X"
+                if (r == 0 or r == rows-1 or c == 0 or c == cols-1) and board[r][c] == "O":
+                    dfs(r, c, "O", "T")
         
-                     
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == "O":
+                    dfs(r, c, "O", "X")
+        
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == "T":
+                    dfs(r, c, "T", "O")
+        
 
+        
+        
 
+                
+                
+        

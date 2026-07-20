d842b6248e5c5dcbfa48dbe1e749c2231bd0bd05
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-07-20 10:28:27 +0530

    Time: 19 ms (96.65%), Space: 22.2 MB (52.58%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,24 +1,23 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        pac_set, atl_set = set(), set()
         rows, cols = len(heights), len(heights[0])
+        pac_set, atl_set = set(), set()
 
-        def dfs(r, c, ocean_set, prev_height):
-
+        def dfs(r, c, visit_set, prev_height):
             if (not (0 <= r < rows) or
-                not (0 <= c < cols) or 
-                prev_height > heights[r][c] or
-                (r,c) in ocean_set):
-                return 
-            
-            ocean_set.add((r,c))
+                not (0 <= c < cols) or
+                (r,c) in visit_set or
+                heights[r][c] < prev_height):
+                return
 
-            dfs(r+1, c, ocean_set, heights[r][c])
-            dfs(r-1, c, ocean_set, heights[r][c])
-            dfs(r, c+1, ocean_set, heights[r][c])
-            dfs(r, c-1, ocean_set, heights[r][c])
+            visit_set.add((r,c))
+
+            dfs(r+1,c, visit_set, heights[r][c]) 
+            dfs(r-1,c, visit_set, heights[r][c]) 
+            dfs(r,c+1, visit_set, heights[r][c]) 
+            dfs(r,c-1, visit_set, heights[r][c]) 
             return
-        
+
         for r in range(rows):
             dfs(r, 0, pac_set, 0)
             dfs(r, cols-1, atl_set, 0)
@@ -27,8 +26,8 @@ class Solution:
             dfs(0, c, pac_set, 0)
             dfs(rows-1, c, atl_set, 0)
         
-        return list(pac_set & atl_set)
-
+        res = []
+        for (r,c) in pac_set & atl_set:
+            res.append([r,c])
         
-        
-
+        return res

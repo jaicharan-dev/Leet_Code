25e0842fc138182503674e9e08927031be8400d4
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-07-21 10:53:35 +0530

    Time: 71 ms (72.97%), Space: 27.6 MB (7.75%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,28 +1,25 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        if endWord not in wordList:
-            return 0
-        
         adj_list = defaultdict(list)
         for word in wordList:
             for i in range(len(word)):
                 pattern = word[:i] + "*" + word[i+1:]
                 adj_list[pattern].append(word)
+        
+        q = deque([(beginWord, 1)])
+        visit = set([beginWord])
 
-        queue = deque([(beginWord, 1)])
-        visited = set([beginWord])
-
-        while queue:
-            curr_word, steps = queue.popleft()
-            if curr_word == endWord:
+        while q:
+            curr, steps = q.popleft()
+            if curr == endWord:
                 return steps
-            
-            for i in range(len(curr_word)):
-                pattern = curr_word[:i] + "*" + curr_word[i+1:]
+
+            for i in range(len(curr)):
+                pattern = curr[:i] + "*" + curr[i+1:]
                 for neighbor in adj_list[pattern]:
-                    if neighbor not in visited:
-                        visited.add(neighbor)
-                        queue.append((neighbor, steps+1))
-                
+                    if neighbor not in visit:
+                        visit.add(neighbor)
+                        q.append((neighbor, steps+1))
                 adj_list[pattern] = []
+        
         return 0

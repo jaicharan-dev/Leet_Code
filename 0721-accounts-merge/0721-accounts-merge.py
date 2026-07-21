2b49e621c8a700c536d4e8324bc0ee8b2c209756
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-07-21 12:31:17 +0530

    Time: 27 ms (61%), Space: 22.6 MB (48.3%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.py
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -11,21 +11,22 @@ class Solution:
                     email_to_id[email] = id_cnt
                     email_to_name[email] = name
                     id_cnt += 1
-                
+        
         parent = list(range(id_cnt))
-
+        
         def find(i):
             if parent[i] == i:
                 return i
             parent[i] = find(parent[i])
             return parent[i]
-        
+
         def union(i, j):
             root_i = find(i)
             root_j = find(j)
-            if root_i != root_j:
-                parent[root_i] = root_j
 
+            if root_i != root_j:
+                parent[root_j] = root_i
+        
         for account in accounts:
             first_email = account[1]
             first_id = email_to_id[first_email]
@@ -38,10 +39,9 @@ class Solution:
         for email, email_id in email_to_id.items():
             root_id = find(email_id)
             components[root_id].append(email)
-        
+
         res = []
         for root_id, email_list in components.items():
             name = email_to_name[email_list[0]]
             res.append([name] + sorted(email_list))
         return res
-            

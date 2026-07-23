5eec8d94d3237a3243a420b5fc76582c9af54c25
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-07-23 13:06:08 +0530

    Time: 37 ms (6.93%), Space: 19.6 MB (42.08%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -8,16 +8,15 @@ class Solution:
             if total == target:
                 res.append(subset.copy())
                 return
-            if i == len(candidates) or total >= target:
+            if i >= len(candidates) or total > target:
                 return
-
+            
             subset.append(candidates[i])
             dfs(i+1, total + candidates[i])
-
             subset.pop()
             while i+1 < len(candidates) and candidates[i] == candidates[i+1]:
                 i += 1
             dfs(i+1, total)
-
+        
         dfs(0, 0)
         return res

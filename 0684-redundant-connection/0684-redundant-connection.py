34734ff0307ae1424af54d06fe30318d0cc3db7b
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-07-24 10:55:04 +0530

    Time: 2 ms (66.93%), Space: 19.4 MB (94.8%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -9,25 +9,25 @@ class Solution:
                 return node
             parent[node] = find(parent[node])
             return parent[node]
-
+        
         def union(u, v):
             root_u = find(u)
             root_v = find(v)
 
             if root_u != root_v:
-                if rank[root_u] > rank[root_v]:
-                    parent[root_v] = root_u
-                elif rank[root_v] > rank[root_u]:
+                if rank[root_u] < rank[root_v]:
                     parent[root_u] = root_v
+                elif rank[root_v] < rank[root_u]:
+                    parent[root_v] = root_u
                 else:
-                    parent[root_v] = root_u 
+                    parent[root_v] = root_u
                     rank[root_u] += 1
-                
                 return True
-            
             return False
         
         for u, v in edges:
-            if not union(u,v):
-               return [u, v]   
-        return []
+            if not union(u, v):
+                return [u, v]
+
+
+        

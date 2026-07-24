658bf2456f260a51a42cc74fdda4d9f12d8ce375
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-07-24 12:03:30 +0530

    Time: 31 ms (49.55%), Space: 22.7 MB (47.99%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.py
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -1,47 +1,54 @@
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
-        email_to_id = {}
-        email_to_name = {}
+        email_id = {}
+        email_name = {}
         id_cnt = 0
 
         for account in accounts:
             name = account[0]
-            for email in account[1:]:
-                if email not in email_to_id:
-                    email_to_id[email] = id_cnt
-                    email_to_name[email] = name
+            for i in range(1, len(account)):
+                if account[i] not in email_id:
+                    email_id[account[i]] = id_cnt
+                    email_name[account[i]] = name
                     id_cnt += 1
-        
-        parent = list(range(id_cnt))
+
+        parent = list(range(id_cnt)) 
+        rank = [1] * id_cnt
         
         def find(i):
             if parent[i] == i:
                 return i
             parent[i] = find(parent[i])
             return parent[i]
-
+        
         def union(i, j):
             root_i = find(i)
             root_j = find(j)
 
             if root_i != root_j:
-                parent[root_j] = root_i
-        
+                if rank[root_i] < rank[root_j]:
+                    parent[root_i] = root_j
+                elif rank[root_j] < rank[root_i]:
+                    parent[root_j] = root_i
+                else:
+                    parent[root_j] = root_i
+                    rank[root_i] += 1
+            
         for account in accounts:
             first_email = account[1]
-            first_id = email_to_id[first_email]
-
-            for other_email in account[2:]:
-                other_id = email_to_id[other_email]
+            first_id = email_id[first_email]
+            for i in range(2, len(account)):
+                other_email = account[i]
+                other_id = email_id[other_email]
                 union(first_id, other_id)
         
         components = defaultdict(list)
-        for email, email_id in email_to_id.items():
-            root_id = find(email_id)
+        for email, idx in email_id.items():
+            root_id = find(idx)
             components[root_id].append(email)
 
         res = []
         for root_id, email_list in components.items():
-            name = email_to_name[email_list[0]]
+            name = email_name[email_list[0]]
             res.append([name] + sorted(email_list))
         return res

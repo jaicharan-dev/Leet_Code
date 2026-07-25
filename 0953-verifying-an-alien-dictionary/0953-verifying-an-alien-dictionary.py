a29546ad44fa8456fca1873930aa8b0a32b36d88
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-07-25 21:15:16 +0530

    Time: 0 ms (100%), Space: 19.3 MB (34.33%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -1,20 +1,19 @@
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
-        alien_dict = {char:idx for idx, char in enumerate(order)}
+      dictionary = {char:idx for idx, char in enumerate(order)}
 
-        for i in range(len(words)-1):
-            w1 = words[i]
-            w2 = words[i+1]
+      for i in range(len(words)-1):
+        w1 = words[i]
+        w2 = words[i+1]
 
-            for j in range(min(len(w1), len(w2))):
-                if w1[j] != w2[j]:
-                    if alien_dict[w1[j]] > alien_dict[w2[j]]:
-                        return False    
-                    break
-            
-            else:
-                if len(w1) > len(w2):
-                    return False
-
-        return True
-                
+        for j in range( min(len(w1), len(w2)) ):
+          if w1[j] != w2[j]:
+            if dictionary[w1[j]] > dictionary[w2[j]]:
+              return False
+            break 
+        
+        else:
+          if len(w1) > len(w2):
+            return False
+      
+      return True 

5516ef9ccc866d1264bf9450231e6a8f26b2a910
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-07-27 16:25:25 +0530

    Time: 502 ms (64.49%), Space: 19.6 MB (52.33%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,13 +1,10 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        if amount == 0: return 0
-
-        dp = [float("inf")] * (amount+1)
+        dp = [amount+1] * (amount+1)
         dp[0] = 0
-
-        for i in range(1, len(dp)):
+        for i in range(1, amount+1):
             for c in coins:
                 if i-c >= 0:
                     dp[i] = min(dp[i], 1 + dp[i-c])
         
-        return dp[amount] if dp[amount] != float("inf") else -1
+        return dp[amount] if dp[amount] != amount+1 else -1

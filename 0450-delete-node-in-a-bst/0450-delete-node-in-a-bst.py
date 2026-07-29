6a1b34fcea9ca8e9bfcebc75f3b72671074d21ff
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-07-29 14:01:07 +0530

    Time: 0 ms (100%), Space: 21.9 MB (77.26%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
@@ -6,12 +6,10 @@
 #         self.right = right
 class Solution:
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
-        
-        if not root:
-            return None
-        
+        if not root: return None
+    
         if root.val == key:
-            if not root.left and not root.right:
+            if not root.right and not root.left:
                 return None
             elif not root.left:
                 return root.right
@@ -25,10 +23,11 @@ class Solution:
                 root.val = son.val
                 root.right = self.deleteNode(root.right, son.val)
                 return root
-
+            
         elif root.val < key:
             root.right = self.deleteNode(root.right, key)
         else:
             root.left = self.deleteNode(root.left, key)
-        
         return root
+
+

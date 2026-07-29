cd6da4620b63562bd1b3559c606ec39f43e7e73a
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-07-29 15:30:27 +0530

    Time: 3 ms (34.94%), Space: 20.2 MB (96.64%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
@@ -5,25 +5,26 @@
 #         self.next = next
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        # if k exist in the first place, needs to be checked and then we can thinki of swapping
         dummy = ListNode(0, head)
-        gL = dummy
-        
-        while True:
-            gN = gL
-            for _ in range(k):
-                gN = gN.next
-                if not gN:
-                    return dummy.next
-                    
-            curr = gL.next
+        gp = dummy
+
+        def is_k(node):
+            count = 0
+            while node.next:
+                node = node.next
+                count += 1
+                if count == k:
+                    return True
+            return False
+
+        while is_k(gp):
+            curr = gp.next
             for _ in range(k-1):
                 temp = curr.next
                 curr.next = temp.next
-                temp.next = gL.next
-                gL.next = temp
-            gL = curr
-            
-            
-
-
-
+                temp.next = gp.next
+                gp.next = temp
+            gp = curr
+        
+        return dummy.next

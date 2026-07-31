43bdf3f01527e76e3b55addef15bc6930961d56d
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-07-31 20:26:41 +0530

    Time: 307 ms (78.64%), Space: 20.5 MB (26%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.py
+++ 0752-open-the-lock/0752-open-the-lock.py
@@ -1,32 +1,32 @@
 class Solution:
     def openLock(self, deadends: List[str], target: str) -> int:
-        visit = set(deadends)
-        if "0000" in visit:
+        
+        deadlock = set(deadends)
+        if "0000" in deadlock:
             return -1
 
-        visit.add("0000")
         q = deque([("0000", 0)])
+        deadlock.add("0000")
 
         while q:
-            state, turns = q.popleft()
+            state, step = q.popleft()
             if state == target:
-                return turns
-            
+                return step
+
             for i in range(4):
                 digit = int(state[i])
+                
+                up_digit = str((digit - 1) % 10) 
+                down_digit = str((digit + 1) % 10)
 
-                up_digit = str((digit + 1) % 10)
-                down_digit = str((digit - 1) % 10)
-
-                up_state = state[:i] + up_digit + state[i+1:]
+                up_state = state[:i] + up_digit + state[i+1:] 
                 down_state = state[:i] + down_digit + state[i+1:]
 
-                if up_state not in visit:
-                    visit.add(up_state)
-                    q.append((up_state, turns+1))
-                
-                if down_state not in visit:
-                    visit.add(down_state)
-                    q.append((down_state, turns+1))
-                
+                if up_state not in deadlock:
+                    q.append((up_state, step+1))
+                    deadlock.add(up_state)
+                if down_state not in deadlock:
+                    q.append((down_state, step+1))
+                    deadlock.add(down_state)
+
         return -1

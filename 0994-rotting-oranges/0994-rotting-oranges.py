ccb1a5dcd4ce54ff67e3827ed29e594030ee9485
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-08-03 14:16:27 +0530

    Time: 4 ms (36.86%), Space: 19.2 MB (96.08%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,29 +1,31 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
-        fresh = 0
-        time = 0
 
-        queue = deque()
+        q = deque()
+        fresh = 0
+
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == 1:
+                if grid[r][c] == 2:
+                    q.append((r, c))
+                elif grid[r][c] == 1:
                     fresh += 1
-                elif grid[r][c] == 2:
-                    queue.append((r,c))
-                    
-        directions = [(1,0), (-1,0), (0,1), (0,-1)]
-        while queue and fresh:
+        time = 0
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        while q and fresh:
             time += 1
-            for _ in range(len(queue)): 
-                row, col = queue.popleft()
+            for _ in range(len(q)):
+                r, c = q.popleft()
                 for dr, dc in directions:
-                    nr, nc = row+dr, col+dc
-                    if ((0 <= nr < rows) and (0 <= nc < cols)
-                        and grid[nr][nc] == 1):
-                        grid[nr][nc] = 2
-                        fresh -= 1
-                        queue.append((nr, nc))
-            
+                    nr, nc = r+dr, c+dc
+                    if (not 0 <= nr < rows or
+                        not 0 <= nc < cols or
+                        grid[nr][nc] != 1):
+                        continue
+                    grid[nr][nc] = 2
+                    q.append((nr, nc))
+                    fresh -= 1
         return time if fresh == 0 else -1
-            
+
+

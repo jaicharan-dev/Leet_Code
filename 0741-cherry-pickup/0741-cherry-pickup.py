ea6dd071367422a00287c37ca41f700bb47da6f4
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-08-23 13:25:16 +0530

    Time: 430 ms (46.43%), Space: 27.1 MB (72.29%) - LeetHub

--- 0741-cherry-pickup/0741-cherry-pickup.py
+++ 0741-cherry-pickup/0741-cherry-pickup.py
@@ -5,22 +5,22 @@ class Solution:
 
         def dp(r1, c1, r2):
             c2 = r1 + c1 - r2
-            
+        
             if (r1 >= n or c1 >= n or r2 >= n or c2 >= n or
                 grid[r1][c1] == -1 or grid[r2][c2] == -1):
-                return float('-inf')
+                return -float('inf')
             
             if r1 == n-1 and c1 == n-1:
                 return grid[n-1][n-1]
             
-            if memo[r1][c1][r2] != None:
+            if memo[r1][c1][r2] is not None:
                 return memo[r1][c1][r2]
             
             if r1 == r2 and c1 == c2:
                 cherries = grid[r1][c1]
             else:
                 cherries = grid[r1][c1] + grid[r2][c2]
-
+            
             max_future = max(
                 dp(r1+1, c1, r2+1),
                 dp(r1+1, c1, r2),
@@ -31,6 +31,6 @@ class Solution:
             ans = cherries + max_future
             memo[r1][c1][r2] = ans
             return ans
-
+        
         result = dp(0, 0, 0)
         return max(0, result)

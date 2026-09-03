3d90c3da9bdab5f6b5a7888b63bf865604111204
Jaicharan Yeluri <jaicharan.dev@gmail.com>
2026-09-03 11:38:37 +0530

    Time: 49 ms (54.13%), Space: 22.1 MB (75.31%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.py
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.py
@@ -3,20 +3,15 @@ class Solution:
         if not matrix or not matrix[0]:
             return 0
         
-        m = len(matrix)
-        n = len(matrix[0])
-
-        dp = [[0] * n for _ in range(m)]
+        m, n = len(matrix), len(matrix[0])
         total_squares = 0
 
         for r in range(m):
             for c in range(n):
                 if matrix[r][c] == 1:
-                    if r == 0 or c == 0:
-                        dp[r][c] = 1
-                    else:
-                        dp[r][c] = min(dp[r-1][c], dp[r-1][c-1], dp[r][c-1]) + 1
+                    if r > 0 and c > 0:
+                        matrix[r][c] = min(matrix[r-1][c], matrix[r-1][c-1], matrix[r][c-1]) + 1
                     
-                    total_squares += dp[r][c]
-
+                    total_squares += matrix[r][c]
+        
         return total_squares
